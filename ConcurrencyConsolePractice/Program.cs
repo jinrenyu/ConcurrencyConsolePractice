c3b83b6d00ea8ff2153c0966df1bd17b9c434603
renyu.jin <renyu.jin@litens.com>
2021-07-27 16:07:42 +0800

    更新

--- ConcurrencyConsolePractice/Program.cs
+++ ConcurrencyConsolePractice/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System;
 
 namespace ConcurrencyConsolePractice
 {
@@ -6,7 +8,31 @@ namespace ConcurrencyConsolePractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            //1.new方式实例化一个Task，需要通过Start方法启动
+            // Task task = new Task(() =>
+            // {
+            //     Thread.Sleep(100);
+            //     Console.WriteLine($"hello, task1的线程ID为{Thread.CurrentThread.ManagedThreadId}");
+            // });
+            // task.Start();
+
+            //2.Task.Factory.StartNew(Action action)创建和启动一个Task
+            // Task task2 = Task.Factory.StartNew(() =>
+            //   {
+            //       Thread.Sleep(100);
+            //       Console.WriteLine($"hello, task2的线程ID为{ Thread.CurrentThread.ManagedThreadId}");
+            //   });
+
+            //3.Task.Run(Action action)将任务放在线程池队列，返回并启动一个Task
+            // Task task3 = Task.Run(() =>
+            //   {
+            //       //Thread.Sleep(100);
+            //       Console.WriteLine($"hello, task3的线程ID为{ Thread.CurrentThread.ManagedThreadId}");
+            //   });
+            Task.FromResult("121");
+            Console.WriteLine($"执行主线程！{ Thread.CurrentThread.ManagedThreadId}");
+            Console.ReadKey();
         }
     }
 }
